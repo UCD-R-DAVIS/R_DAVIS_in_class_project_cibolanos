2343f7210c05c4860ca6274f6b5fb6d0ffb0e5d4
Chloe Bolanos <chloebolanos@campus-021-191.ucdavis.edu>
2024-10-08 13:43:05 -0700

    Homework 2_ Chloe Bolanos

---
+++ week_2_homework.R
@@ -0,0 +1,15 @@
+set.seed(15)
+hw2 <- runif(50, 4, 50)
+hw2 <- replace(hw2, c(4,12,22,27), NA)
+hw2
+!is.na(hw2)
+hw2[!is.na(hw2)]
+prob1 <-hw2[!is.na(hw2)]
+prob1 <- prob1[prob1>14 & prob1<38]
+prob1
+times3 <-prob1*3
+times3
+plus10 <-times3+10
+plus10
+final <- plus10[c(TRUE,FALSE)]
+final
