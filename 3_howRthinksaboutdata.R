6b8985206618e0e4072ec218a3f01fa338d40f52
Chloe Bolanos <chloebolanos@Bolanos-MacBook-Air.local>
2024-10-10 14:16:21 -0700

    in class notes for first part of class 101024

---
+++ 3_howRthinksaboutdata.R
@@ -0,0 +1,34 @@
+# Other data types
+## Lists
+c(4,6,"dog")
+a <- list(4,6,"dog")
+class(a)
+str(a)
+
+# Data.frames
+letters
+data.frame(letters)
+df <- data.frame(letters)
+as.data.frame(t(df))
+length(df) # number of columns in a data frame
+dim(df) #rows, columns
+nrow(df)
+ncol(df)
+
+# factors
+animals <- factor(c("duck", "duck", "goose", "goose"))
+animals #levels goes through alphabetically 
+animals <- factor(c("pigs" , "duck", "duck", "goose", "goose"))
+animals
+class(animals)
+levels(animals)
+nlevels(animals)
+
+
+animals <- factor(x = animals, levels = c("goose", "pigs", "duck")) #changes the levels #factor turn it into a set of categorical data
+animals
+
+year <- factor(c(1978,1980,1934,1979))
+year
+class(year)
+as.numeric(year) #order of the levels 
