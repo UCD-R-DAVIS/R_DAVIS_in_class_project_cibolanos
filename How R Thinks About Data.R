f0414bace5c13d7d8df4ec824d29c588799c12e6
Chloe Bolanos <chloebolanos@Bolanos-MacBook-Air.local>
2024-10-10 13:19:18 -0700

    uploaded 101024

--- How R Thinks About Data.R
+++ How R Thinks About Data.R
@@ -1,1 +1,52 @@
 # How R thinks About Data -----
+
+## Vectors ----
+weight_g <- c(50,60,65,82)
+weight_one_value <- c(50)
+
+animals <- c("mouse", "rat", "dog")
+animals
+
+### Inspection ----
+length(weight_g)
+str(weight_g)
+
+
+### Change vectors -----
+weight_g <- c(weight_g,90)
+weight_g
+
+### Challenge ----
+num_char <- c(1, 2, 3, "a") #chooses lowest common denominator
+# vectors have to be the SAME class of values
+num_char
+num_logical <- c(1, 2, 3, TRUE)
+# coerces values to be all the same, e.g. when TRUE is included
+char_logical <- c("a", "b", "c", TRUE)
+char_logical
+
+tricky <- c(1, 2, 3, "4")
+
+
+## Subsetting ----
+animals <- c ("mouse", "rat", "dog", "cat")
+animals
+animals[2]
+animals[c(2,3)]
+
+# indexing: take items from a vector and create a new combination of values
+
+### Conditional subsetting ----
+weight_g <- c(21, 34, 39, 54, 55)
+weight_g > 50
+weight_g[weight_g > 50]
+
+## Symbols
+#%in%
+animals
+#"mouse" "rat" "dog" "cat"
+# %in% within a bucket
+# == pairwise matching -- ORDER MATTERS
+animals %in% c("rat", "cat", "dog", "duck", "goat")
+animals == c("rat", "cat", "dog", "duck", "goat")
+
---
+++ Project Management.R
@@ -0,0 +1,16 @@
+#Project Management
+
+
+
+# Working Directory and File Paths
+getwd()
+"/Users/chloebolanos/Desktop/R-Projects/R_DAVIS_in_class_project_cibolanos"
+
+
+d <- read.csv("./data/tail_length.csv")
+# Code and Data Organization 
+
+## Best Practices ---
+# Treat raw data as read only
+# Treat generated output as disposable
+# dir.create("./lectures")
