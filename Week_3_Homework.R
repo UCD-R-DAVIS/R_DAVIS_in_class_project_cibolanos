076c85c059658dcd1854b3b1478f518dad226fa2
Chloe Bolanos <chloebolanos@campus-008-136.ucdavis.edu>
2024-10-15 13:46:23 -0700

    Week 3 Homework

---
+++ Week_3_Homework.R
@@ -0,0 +1,11 @@
+### Week 3 Homework
+surveys <- read.csv("data/portal_data_joined.csv")
+surveys_base <- surveys [1:5000, c(6,9,13)]
+surveys_base <- surveys_base[complete.cases(surveys_base),]
+surveys_base$species_id <- factor(surveys_base$species_id)
+surveys_base$plot_type <- factor(surveys_base$plot_type)
+levels(surveys_base$species_id)
+typeof(surveys_base$species_id)
+class(surveys_base$species_id)
+challenge_base <-surveys_base[surveys_base[,2]>150,]
+challenge_base
