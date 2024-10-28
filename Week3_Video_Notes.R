074f959f166f135a99f99e297e6733dbee8f6dfa
Chloe Bolanos <chloebolanos@Bolanos-Air.attlocal.net>
2024-10-27 20:29:54 -0700

    Updates Week 3 Video Notes with 3.3

--- Week3_Video_Notes.R
+++ Week3_Video_Notes.R
@@ -43,3 +43,76 @@ heights[!is.na(heights)]
 
 #complete cases will return an object that only has values where there is the full information available
 heights[complete.cases(heights)]
+
+
+### Other Data Types
+
+# Lists - a data type that is constructed of multiple vectors; it can have different data types, data lengths; essentially it's multiple vectors in one object
+
+c(4,6,"dog") #spits out the values all in one row under the same line
+
+list(4,6,"dog") #gives us three separate vectors
+
+a<- list (4,6,"dog")
+class(a)
+str(a)
+
+b <- list(4,letters, "dog")
+str(b)
+length(b)
+length(b[[2]]) #index value for letters
+
+# Data frames #you can have different data variables but they all have to be the same length
+letters
+data.frame(letters)
+df <- data.frame(letters)
+length(df)
+dim(df) #first number is number of rows and then the second number is the number of columns
+nrow(df) #number of rows
+ncol(df) #number of columns
+df2 <- data.frame(letters, letters) 
+str(df2)
+dim(df2)
+
+data.frame(letters,"dog") 
+data.frame(letters,1) 
+data.frame(letters,1:2) #R has decided to recycle one and two until it reaches the 26 for letters; in data frames the lengths have to be the same
+data.frame(letters, 1:3) #error
+
+#Matrices are basically a data frame but it has to be the same exact type of data in the X and the Y
+matrix(nrow = 10, ncol=10)
+matrix(1:10,nrow=10, ncol=10)
+matrix(1:10,nrow=10, ncol=10, byrow=TRUE)
+m <- matrix(1:10,nrow=10, ncol=10, byrow=TRUE) #row is on the left side of the comma, and column is the right side of the comma
+m [1,3]
+m[c(1,2),c(5,6)]
+
+#Arrays are basically matrices but in three dimensions; essentially stacked matrices
+
+#Factors essentially characters but with some sort of level value or order to them
+response <- factor(c("no","yes","maybe","no","maybe","no"))
+class(response)
+levels(response)
+nlevels(response)
+typeof(response)
+response 
+
+response <- factor(response, levels = c("yes","maybe","no"))
+response
+
+#Converting a factor
+# if we want to convert to as.character instead of factor
+as.character(response)
+
+year_fct <- factor(c(1990,1983,1977,1998,1999))
+year_fct
+as.numeric(year_fct)
+as.numeric(as.character(year_fct))
+
+#Renaming the different levels
+levels(response)
+levels(response)[1] <- "YES"
+response
+
+levels(response) <- c("YES", "MAYBE", "NO")
+response
